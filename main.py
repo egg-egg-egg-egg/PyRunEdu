19bdbee03e26a0da7353cb489ae8540dbd4eaece
unknown <1127885315@qq.com>
2024-10-06 23:23:18 +0800

    嵌入jupyterlite

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.set_page_config(layout='wide')
 
 st.write(
     """<iframe
-  src="https://jupyter.org/try-jupyter/lab/"
+  src="https://egg-egg-egg-egg.github.io/"
   width="100%"
   height="700px"
 ></iframe>""",unsafe_allow_html = True
